ef5839dedcd48dc586a81ac97be5185b955b71f9
AbdulrahmanAlTuraigi <137075843+AbdulrahmanAlTuraigi@users.noreply.github.com>
2025-04-18 17:39:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoommateRequestForm from "./pages/RoommateRequestForm";
+import UniversityRedirect from "./pages/UniversityRedirect";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -6,6 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<RoommateRequestForm />} />
+        <Route path="/university" element={<UniversityRedirect />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
