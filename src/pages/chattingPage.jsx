828bb4439b637e42140edc237877628f8e286fdc
Gsty <trroky070@gmail.com>
2025-04-19 22:28:04 +0300

    aa

--- src/pages/chattingPage.jsx
+++ src/pages/chattingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import "./chattingPage.css";
+import '../styles/chattingPage.css';
 
 const defaultUsers = [
   { name: "John", avatar: "👨‍💼", bio: "Software Engineer" },
