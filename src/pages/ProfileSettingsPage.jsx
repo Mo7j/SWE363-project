85f64431b02b06866bc25bff9e86627a5ea0fa0e
Mohammed Fahad Alajmi <153314926+WackAjmi@users.noreply.github.com>
2025-04-19 21:34:56 +0300

    fixed button colors and added margin to the top of the page so the navbar does not hide the content

--- src/pages/ProfileSettingsPage.jsx
+++ src/pages/ProfileSettingsPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styels/profileSettingsPage.css';
 
 const ProfileSettingsPage = () => {
     const [formData, setFormData] = useState({
@@ -16,7 +18,6 @@ const ProfileSettingsPage = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Form Data Submitted:', formData);
-        // Add your form submission logic here
         const isValid = formData.name && formData.lastname && formData.email.endsWith("@kfupm.edu.sa") && formData.bio;
         if (isValid) {
             alert("Changes Saved Successfully");
@@ -30,7 +31,7 @@ const ProfileSettingsPage = () => {
     };
 
     return (
-        <div style={{ maxWidth: '500px', margin: '0 auto', padding: '20px' }}>
+        <div className='profile-settings-page'>
             <h2>Profile Settings</h2>
             <form onSubmit={handleSubmit}>
                 <div style={{ marginBottom: '15px' }}>
@@ -81,8 +82,8 @@ const ProfileSettingsPage = () => {
                         rows="4"
                     />
                 </div>
-                <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#007BFF', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-                    Submit
+                <button type="submit">
+                    Submit Changes
                 </button>
             </form>
         </div>
