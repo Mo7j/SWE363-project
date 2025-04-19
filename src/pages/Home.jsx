df017df282cd487a266f57f66c664b744f7c1775
Mohammed Fahad Alajmi <153314926+WackAjmi@users.noreply.github.com>
2025-04-19 21:41:26 +0300

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-
+import '../styels/homePage.css'; // Assuming you have some styles for the Home component
 export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className='home-page'>
       <h1>Welcome to the Home Page</h1>
       <button onClick={() => navigate('/settings')}>Go to Profile Settings</button>
       <button onClick={() => navigate('/website-settings')}>Go to Website Settings</button>
